3e68f989e0d15a3ed67b539ec3052a52427f95ab
david durost <david.durost@gmail.com>
2015-02-28 16:08:23 -0500

    added controller skeleton

---
+++ Controller/ModelController.php
@@ -0,0 +1,45 @@
+<?php
+namespace RideSocial\Bundle\VehicleBundle\Controller;
+
+class ModelController extends \RideSocial\Bundle\CoreBundle\Controller\Controller
+{
+    /**
+     * Index action
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return mixed
+     */
+    public function indexAction(\Symfony\Component\HttpFoundation\Request $request)
+    {
+        return parent::indexAction($request);
+    }
+    
+    /**
+     * New action
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return mixed
+     */
+    public function newAction(\Symfony\Component\HttpFoundation\Request $request)
+    {
+        return parent::newAction($request);
+    }
+    
+    /**
+     * Show actino
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return mixed
+     */
+    public function showAction(\Symfony\Component\HttpFoundation\Request $request)
+    {
+        return parent::showAction($request);
+    }
+    
+    /**
+     * Search action
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return mixed
+     */
+    public function searchAction(\Symfony\Component\HttpFoundation\Request $request)
+    {
+        return parent::searchAction($request);
+    }
+}
